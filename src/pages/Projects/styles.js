fd12ca1e5181a099ea6cfb8145aa28652a591dbf
Sarit Vakrat <>
2020-01-01 16:53:52 +0200

    fix styles issues

--- src/pages/Projects/styles.js
+++ src/pages/Projects/styles.js
@@ -1,9 +1,17 @@
-import styled from 'styled-components'
-import { Content } from 'carbon-components-react/lib/components/UIShell'
 
-export const StyledContent = styled(Content)`
-  min-height: 100vh;
-  @media (max-width: 640px) {
-    margin-left: 0 !important;
-  }
+import styled from 'styled-components'
+import { white } from '@carbon/colors'
+
+export const ProjectItem = styled.li`
+  margin-top: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid ${white};
+`
+
+export const ProjectTitle = styled.h4`
+  font-weight: bold;
+`
+
+export const SkillContainer = styled.div`
+  margin-top: 1.2rem;
 `
