548c2b4b8dc51310fd50c0ef06d6a9bfc25fac52
Sarit Vakrat <>
2020-01-01 17:35:31 +0200

    try fix deployment

---
+++ build/precache-manifest.658921cdeda2851f736f0ea8c75b0be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f39e37068c30ba6d1ff4c071635e7f3",
+    "url": "/sarit-online-portfolio/index.html"
+  },
+  {
+    "revision": "d347cddd4a730d42281f",
+    "url": "/sarit-online-portfolio/static/css/main.35f26896.chunk.css"
+  },
+  {
+    "revision": "74855e0619fdef7dda5d",
+    "url": "/sarit-online-portfolio/static/js/2.a18b7e05.chunk.js"
+  },
+  {
+    "revision": "f05bfc683a175306602e6f609a238cdb",
+    "url": "/sarit-online-portfolio/static/js/2.a18b7e05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d347cddd4a730d42281f",
+    "url": "/sarit-online-portfolio/static/js/main.d2c6faef.chunk.js"
+  },
+  {
+    "revision": "43df77288d525a2ee5f4",
+    "url": "/sarit-online-portfolio/static/js/runtime-main.95c90fb5.js"
+  }
+]);
