aef5778b42f5ad4136ded05ea00a97299bab5ed6
Sarit Vakrat <>
2020-01-02 12:12:57 +0200

    Updates

---
+++ precache-manifest.e868f6e2e9e0fbdb22b76079f1c960b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03453089fe1957aed750b43645475525",
+    "url": "/sarit-online-portfolio/index.html"
+  },
+  {
+    "revision": "54bc65d60f6557e4184c",
+    "url": "/sarit-online-portfolio/static/css/main.35f26896.chunk.css"
+  },
+  {
+    "revision": "5962698b1b5b92a629c9",
+    "url": "/sarit-online-portfolio/static/js/2.33dbb850.chunk.js"
+  },
+  {
+    "revision": "f05bfc683a175306602e6f609a238cdb",
+    "url": "/sarit-online-portfolio/static/js/2.33dbb850.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54bc65d60f6557e4184c",
+    "url": "/sarit-online-portfolio/static/js/main.9c937b57.chunk.js"
+  },
+  {
+    "revision": "43df77288d525a2ee5f4",
+    "url": "/sarit-online-portfolio/static/js/runtime-main.95c90fb5.js"
+  }
+]);
--- src/components/UserHeader/index.js
+++
@@ -1,4 +0,0 @@
-import UserHeader from './UserHeader'
-
-/** displays your general details at the top of every page. It includes your basic information, I used my avatar, my name, username, job title, location, years of experience, my headline, and my articles blog */
-export default UserHeader
